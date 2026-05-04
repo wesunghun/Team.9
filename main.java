ac8a72b778abc971c9fa781d438e3f74a9803b0a
Dongkyun-Kim <gjffoddl610@gmail.com>
2026-05-04 22:49:15 +0900

    연습

---
+++ main.java
@@ -0,0 +1,16 @@
+
+/**
+ * main 클래스의 설명을 작성하세요.
+ *
+ * @author (작성자 이름)
+ * @version (버전 번호 또는 작성한 날짜)
+ */
+public class main
+{
+    public static void main(String[] args){
+        Student s1 = new Student("Jenna" , "20240001" , 4);
+        s1.calculateGugudan();
+        
+        System.out.println();
+    }
+}
