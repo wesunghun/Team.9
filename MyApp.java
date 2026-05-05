cf683e3cf1357b4cdd1b5a9fcf806074a699b017
wesunghun <hun12345689@gamil.com>
2026-05-05 17:54:10 +0900

    거의 완성

--- MyApp.java
+++ MyApp.java
@@ -10,21 +10,25 @@ public class MyApp
 {
     public static void main(String[] args){
         Student jenna = new Student("Jenna", 2005);
-        Student john = new Student("John");
-        Student maria = new Student(2029);
-        Student james = new Student();
+        Student john = new Student("John", 2010);
+        Student maria = new Student("Maria", 2029);
+        Student james = new Student("James", 2009);
         
         Scanner scan = new Scanner(System.in);
-        System.out.print("누구에게 몇 단을 물어보시겠습니까?(이름과 단수를 공백으로 분리하여 입력하세요.)");
+        System.out.println("누구에게 몇 단을 물어보시겠습니까?");
         String nameInput = scan.next();
-        String danInput = scan.nextInt();
-        while(true){
+        int danNum = scan.nextInt();
+        
         if (nameInput.equals("Jenna")){
-            jenna.calculateGugudan();
-            break;
+        jenna.calculateGugudan(danNum);
+        } else if(nameInput.equals("John")){
+            john.calculateGugudan(danNum);
+        } else if(nameInput.equals("Maria")){
+            maria.calculateGugudan(danNum);
+        } else if(nameInput.equals("James")){
+            james.calculateGugudan(danNum);
         } else{
-            System.out.println("이름을 다시 입력하십시오.");
-        }
+            System.out.println("없는 학생입니다.");
         }
     }
 }
--- Student.java
+++ Student.java
@@ -22,53 +22,18 @@ public class Student
     }
     
     /**
-     * Student 클래스의 객체 생성자
-     */
-    public Student(String name)
-    {
-        // 인스턴스 변수 초기화
-        this.name = name;
-    }
-    
-    /**
-     * Student 클래스의 객체 생성자
-     */
-    public Student(int stID)
-    {
-        // 인스턴스 변수 초기화
-        this.stID = stID;
-    }
-    
-    /**
-     * Student 클래스의 객체 생성자
-     */
-    public Student()
-    {
-        // 인스턴스 변수 초기화
-        
-    }
-    
-    /**
      * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
      *
      * @param  y  메소드의 샘플 파라미터
      * @return    x 더하기 y의 결과값을 반환
      */
-<<<<<<< HEAD
-    public void calculateGugudan()
-    {
-        // 여기에 코드를 작성하세요.
-        for (int row = 1; row < 10; row++){
-            for (int col = 1; col < 10; col++){
-                System.out.print(row + "x" + col + "=" + (row * col));
-            }
-        }
-        System.out.println();
-=======
     public void calculateGugudan(int dan)
     {
         // 여기에 코드를 작성하세요.
-        return x + y;
->>>>>>> a383ade45bd395fc2faaeb37052f197379bf55aa
+        for (int i = 1; i < 10; i++){
+            System.out.print(dan + "x" + i + "=" + (dan * i));
+            System.out.print("\t");
+        }
+        System.out.println();
+        }
     }
-}
